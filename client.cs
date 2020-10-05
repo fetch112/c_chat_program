8d8859985cbd0854a7ad682d520e708268c1697f
kimruru <aszqw65@naver.com>
2020-10-05 12:01:35 +0900

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream stream = default(NetworkStream);
+        string message = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = Encoding.Unicode.GetBytes(this.textBox1.Text + "$");
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+            this.textBox1.Text = "";
+            this.textBox1.Focus();
+        }
+        private void connect_Click(object sender, EventArgs e)
+        {
+            /*if 접속되어 있으면... 재접속 안됨 추가하기*/
+            clientSocket.Connect("여기에 아이피 넣으세요", 9999); 
+            stream = clientSocket.GetStream();
+
+            message = "Connected to Chat Server";
+            DisplayText(message);
+
+            byte[] buffer = Encoding.Unicode.GetBytes(this.textBox2.Text + "$");
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+
+            Thread t_handler = new Thread(GetMessage);
+            t_handler.IsBackground = true;
+            t_handler.Start();
+        }
+        private void GetMessage()
+
+        {
+            while (true)
+            {
+                stream = clientSocket.GetStream();
+                int BUFFERSIZE = clientSocket.ReceiveBufferSize;
+                byte[] buffer = new byte[BUFFERSIZE];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+
+                string message = Encoding.Unicode.GetString(buffer, 0, bytes);
+                DisplayText(message);
+            }
+        }
+        private void DisplayText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.BeginInvoke(new MethodInvoker(delegate
+                {
+                    richTextBox1.AppendText(text + Environment.NewLine);
+                    FlashWindow.Flash(this);
+                }));
+            }
+            else
+                richTextBox1.AppendText(text + Environment.NewLine);
+                FlashWindow.Flash(this);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            message = "Connected to Chat Server";
+            Close();
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnsend_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+        }
+        private void Press_enter(object sender, KeyEventArgs e)
+        {
+            if(textBox1.Text != "")
+            { 
+                if (e.KeyCode == Keys.Enter)
+                {
+
+                    button2_Click(sender, e);
+                    e.SuppressKeyPress = true;  
+                }
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+    class FlashWindow
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct FLASHWINFO
+        {
+            /// <summary>
+            /// The size of the structure in bytes.
+            /// </summary>
+            public uint cbSize;
+            /// <summary>
+            /// A Handle to the Window to be Flashed. The window can be either opened or minimized.
+            /// </summary>
+            public IntPtr hwnd;
+            /// <summary>
+            /// The Flash Status.
+            /// </summary>
+            public uint dwFlags;
+            /// <summary>
+            /// The number of times to Flash the window.
+            /// </summary>
+            public uint uCount;
+            /// <summary>
+            /// The rate at which the Window is to be flashed, in milliseconds. If Zero, the function uses the default cursor blink rate.
+            /// </summary>
+            public uint dwTimeout;
+        }
+
+        /// <summary>
+        /// Stop flashing. The system restores the window to its original stae.
+        /// </summary>
+        public const uint FLASHW_STOP = 0;
+
+        /// <summary>
+        /// Flash the window caption.
+        /// </summary>
+        public const uint FLASHW_CAPTION = 1;
+
+        /// <summary>
+        /// Flash the taskbar button.
+        /// </summary>
+        public const uint FLASHW_TRAY = 2;
+
+        /// <summary>
+        /// Flash both the window caption and taskbar button.
+        /// This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags.
+        /// </summary>
+        public const uint FLASHW_ALL = 3;
+
+        /// <summary>
+        /// Flash continuously, until the FLASHW_STOP flag is set.
+        /// </summary>
+        public const uint FLASHW_TIMER = 4;
+
+        /// <summary>
+        /// Flash continuously until the window comes to the foreground.
+        /// </summary>
+        public const uint FLASHW_TIMERNOFG = 12;
+
+
+        /// <summary>
+        /// Flash the spacified Window (Form) until it recieves focus.
+        /// </summary>
+        /// <param name="form">The Form (Window) to Flash.</param>
+        /// <returns></returns>
+        public static bool Flash(System.Windows.Forms.Form form)
+        {
+            // Make sure we're running under Windows 2000 or later
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL | FLASHW_TIMERNOFG, uint.MaxValue, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        private static FLASHWINFO Create_FLASHWINFO(IntPtr handle, uint flags, uint count, uint timeout)
+        {
+            FLASHWINFO fi = new FLASHWINFO();
+            fi.cbSize = Convert.ToUInt32(Marshal.SizeOf(fi));
+            fi.hwnd = handle;
+            fi.dwFlags = flags;
+            fi.uCount = count;
+            fi.dwTimeout = timeout;
+            return fi;
+        }
+
+        /// <summary>
+        /// Flash the specified Window (form) for the specified number of times
+        /// </summary>
+        /// <param name="form">The Form (Window) to Flash.</param>
+        /// <param name="count">The number of times to Flash.</param>
+        /// <returns></returns>
+        public static bool Flash(System.Windows.Forms.Form form, uint count)
+        {
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL, count, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Start Flashing the specified Window (form)
+        /// </summary>
+        /// <param name="form">The Form (Window) to Flash.</param>
+        /// <returns></returns>
+        public static bool Start(System.Windows.Forms.Form form)
+        {
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL, uint.MaxValue, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Stop Flashing the specified Window (form)
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        public static bool Stop(System.Windows.Forms.Form form)
+        {
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_STOP, uint.MaxValue, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// A boolean value indicating whether the application is running on Windows 2000 or later.
+        /// </summary>
+        private static bool Win2000OrLater
+        {
+            get { return System.Environment.OSVersion.Version.Major >= 5; }
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ server.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+/*비동기 1:n 통신*/
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+
+        TcpListener server = null; // 서버
+        TcpClient clientSocket = null; // TCP 네트워크 클라이언트에서 연결에 대 한 수신 대기합니다.
+        static int counter = 0; // 사용자 수
+     
+        public Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>();
+        // 각 클라이언트 마다 리스트에 추가
+        public MainForm()
+        {
+            InitializeComponent(); //메인 컴포넌트 초기화
+            //socket start
+            Thread t = new Thread(InitSocket); //비동기 1:n 스레드 소켓 생성
+            t.IsBackground = true; //스래드 백그라운드 true
+            t.Start(); // 스레드 사지가1
+        }
+
+        private void InitSocket() //소켓 초기화 그리고 서버 받기
+        {
+            server = new TcpListener(IPAddress.Any, 9999); //
+            clientSocket = default(TcpClient);
+            server.Start();
+            DisplayText(">> Server Started"); //서버가 시작되면 서버 화면에 시작된 화면 보여주기
+
+            while (true)
+            {
+                try
+                {
+                    counter++;
+                    clientSocket = server.AcceptTcpClient();
+                    DisplayText(">> Accept connection from client");
+
+                    NetworkStream stream = clientSocket.GetStream();
+                    byte[] buffer = new byte[1048576];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string user_name = Encoding.Unicode.GetString(buffer, 0, bytes);
+                    user_name = user_name.Substring(0, user_name.IndexOf("$"));
+
+                    clientList.Add(clientSocket, user_name);
+
+                    // send message all user
+                    SendMessageAll(user_name + " Joined ", "", false);
+
+                    handleClient h_client = new handleClient();
+                    h_client.OnReceived += new handleClient.MessageDisplayHandler(OnReceived);
+                    h_client.OnDisconnected += new handleClient.DisconnectedHandler(h_client_OnDisconnected);
+                    h_client.startClient(clientSocket, clientList);
+                }
+                catch (SocketException se)
+                {
+                    Trace.WriteLine(string.Format("InitSocket - SocketException : {0}", se.Message));
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(string.Format("InitSocket - Exception : {0}", ex.Message));
+                    break;
+                }
+            }
+
+            clientSocket.Close();
+            server.Stop();
+        }
+        void h_client_OnDisconnected(TcpClient clientSocket)
+        {
+            if (clientList.ContainsKey(clientSocket))
+                clientList.Remove(clientSocket);
+        }
+        private void OnReceived(string message, string user_name) //유저 접속
+        {
+            string displayMessage = "From client : " + user_name + " : " + message;
+            DisplayText(displayMessage);
+            SendMessageAll(message, user_name, true);
+        }
+        public void SendMessageAll(string message, string user_name, bool flag)
+        {
+            foreach (var pair in clientList)
+            {
+                Trace.WriteLine(string.Format("tcpclient : {0} user_name : {1}", pair.Key, pair.Value));
+
+                TcpClient client = pair.Key as TcpClient;
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = null;
+
+                if (flag)
+                {
+                    buffer = Encoding.Unicode.GetBytes(user_name + "[" + DateTime.Now.ToString("hh:mm:ss") + "] : " + message); //클라이언트에게 메세지 보내기
+                }  //버퍼에 유니코드로 인코딩된 겟 바이트스트림 메세지 보내기
+                else
+                {
+                    buffer = Encoding.Unicode.GetBytes(message);
+                }
+
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+
+            }
+        }
+        private void DisplayText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.BeginInvoke(new MethodInvoker(delegate { richTextBox1.AppendText(text + Environment.NewLine); }));
+            }
+            else
+                richTextBox1.AppendText(text + Environment.NewLine);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    class FlashWindow
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct FLASHWINFO
+        {
+            /// <summary>
+            /// The size of the structure in bytes.
+            /// </summary>
+            public uint cbSize;
+            /// <summary>
+            /// A Handle to the Window to be Flashed. The window can be either opened or minimized.
+            /// </summary>
+            public IntPtr hwnd;
+            /// <summary>
+            /// The Flash Status.
+            /// </summary>
+            public uint dwFlags;
+            /// <summary>
+            /// The number of times to Flash the window.
+            /// </summary>
+            public uint uCount;
+            /// <summary>
+            /// The rate at which the Window is to be flashed, in milliseconds. If Zero, the function uses the default cursor blink rate.
+            /// </summary>
+            public uint dwTimeout;
+        }
+
+        /// <summary>
+        /// Stop flashing. The system restores the window to its original stae.
+        /// </summary>
+        public const uint FLASHW_STOP = 0;
+
+        /// <summary>
+        /// Flash the window caption.
+        /// </summary>
+        public const uint FLASHW_CAPTION = 1;
+
+        /// <summary>
+        /// Flash the taskbar button.
+        /// </summary>
+        public const uint FLASHW_TRAY = 2;
+
+        /// <summary>
+        /// Flash both the window caption and taskbar button.
+        /// This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags.
+        /// </summary>
+        public const uint FLASHW_ALL = 3;
+
+        /// <summary>
+        /// Flash continuously, until the FLASHW_STOP flag is set.
+        /// </summary>
+        public const uint FLASHW_TIMER = 4;
+
+        /// <summary>
+        /// Flash continuously until the window comes to the foreground.
+        /// </summary>
+        public const uint FLASHW_TIMERNOFG = 12;
+
+
+        /// <summary>
+        /// Flash the spacified Window (Form) until it recieves focus.
+        /// </summary>
+        /// <param name="form">The Form (Window) to Flash.</param>
+        /// <returns></returns>
+        public static bool Flash(System.Windows.Forms.Form form)
+        {
+            // Make sure we're running under Windows 2000 or later
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL | FLASHW_TIMERNOFG, uint.MaxValue, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        private static FLASHWINFO Create_FLASHWINFO(IntPtr handle, uint flags, uint count, uint timeout)
+        {
+            FLASHWINFO fi = new FLASHWINFO();
+            fi.cbSize = Convert.ToUInt32(Marshal.SizeOf(fi));
+            fi.hwnd = handle;
+            fi.dwFlags = flags;
+            fi.uCount = count;
+            fi.dwTimeout = timeout;
+            return fi;
+        }
+
+        /// <summary>
+        /// Flash the specified Window (form) for the specified number of times
+        /// </summary>
+        /// <param name="form">The Form (Window) to Flash.</param>
+        /// <param name="count">The number of times to Flash.</param>
+        /// <returns></returns>
+        public static bool Flash(System.Windows.Forms.Form form, uint count)
+        {
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL, count, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Start Flashing the specified Window (form)
+        /// </summary>
+        /// <param name="form">The Form (Window) to Flash.</param>
+        /// <returns></returns>
+        public static bool Start(System.Windows.Forms.Form form)
+        {
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL, uint.MaxValue, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Stop Flashing the specified Window (form)
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        public static bool Stop(System.Windows.Forms.Form form)
+        {
+            if (Win2000OrLater)
+            {
+                FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_STOP, uint.MaxValue, 0);
+                return FlashWindowEx(ref fi);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// A boolean value indicating whether the application is running on Windows 2000 or later.
+        /// </summary>
+        private static bool Win2000OrLater
+        {
+            get { return System.Environment.OSVersion.Version.Major >= 5; }
+        }
+
+
+    }
+    class handleClient
+    {
+        TcpClient clientSocket = null;
+        public Dictionary<TcpClient, string> clientList = null;
+
+        public void startClient(TcpClient clientSocket, Dictionary<TcpClient, string> clientList)
+        {
+            this.clientSocket = clientSocket;
+            this.clientList = clientList;
+
+            Thread t_hanlder = new Thread(doChat);
+            t_hanlder.IsBackground = true;
+            t_hanlder.Start();
+        }
+        public delegate void MessageDisplayHandler(string message, string user_name);
+        public event MessageDisplayHandler OnReceived;
+        public delegate void DisconnectedHandler(TcpClient clientSocket);
+        public event DisconnectedHandler OnDisconnected;
+        private void doChat()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                byte[] buffer = new byte[1024];
+                string msg = string.Empty;
+                int bytes = 0;
+                int MessageCount = 0;
+
+                while (true)
+                {
+                    MessageCount++;
+                    stream = clientSocket.GetStream();
+                    bytes = stream.Read(buffer, 0, buffer.Length);
+                    msg = Encoding.Unicode.GetString(buffer, 0, bytes);
+                    msg = msg.Substring(0, msg.IndexOf("$"));
+
+                    if (OnReceived != null)
+                        OnReceived(msg, clientList[clientSocket].ToString());
+                }
+            }
+            catch (SocketException se)
+            {
+                Trace.WriteLine(string.Format("doChat - SocketException : {0}", se.Message));
+
+                if (clientSocket != null)
+                {
+                    if (OnDisconnected != null)
+                        OnDisconnected(clientSocket);
+
+                    clientSocket.Close();
+                    stream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(string.Format("doChat - Exception : {0}", ex.Message));
+
+                if (clientSocket != null)
+                {
+                    if (OnDisconnected != null)
+                        OnDisconnected(clientSocket);
+
+                    clientSocket.Close();
+                    stream.Close();
+                }
+            }
+        }
+
+    }
+}
